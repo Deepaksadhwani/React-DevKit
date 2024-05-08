851413b3e630746d15836e224be7082ae1b47422
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-05-08 16:40:40 +0530

    create a React starter package

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className=" flex h-screen items-center justify-center bg-white text-green-800">
+     
+      <Button variant="default" className="font-semibold">Click here</Button>
+    </div>
+  );
+};
+
+export default App;
